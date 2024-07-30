d3a56a2f945f0d956f2b2d6d6cbda58f830b1fb2
pranee08-git <146509734+pranee08-git@users.noreply.github.com>
2024-07-30 13:44:09 +0530

    Delete day3/src directory

--- day3/src/ContactPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ContactPage from './components/pages/ContactPage';
-
-
-const ContactPage = () => {
-    return <div>Contact Page Content</div>;
-}
-
-export default ContactPage;
--- day3/src/RoomsPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const RoomsPage = () => {
-    return <div>Rooms Page Content</div>;
-}
-
-export default RoomsPage;
